19b6a41c27678a91e05d2a5f9703c459b1322033
Rohan-IIT <rpawar3@hawk.iit.edu>
2023-12-03 19:04:08 -0600

    First push

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    // ... other plugins
+    require('autoprefixer'),
+  ],
+}
+
